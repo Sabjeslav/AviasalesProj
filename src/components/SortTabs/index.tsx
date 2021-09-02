4c8d0a6234a005fbd4d5f84ab18259f476b81934
Sabjeslav <s4tenko@gmail.com>
2021-09-02 11:34:13 +0300

    Added styles for sorting tabs

--- src/components/SortTabs/index.tsx
+++ src/components/SortTabs/index.tsx
@@ -1,20 +1,33 @@
-import React from 'react';
+import React, {useState} from 'react';
 import style from './SortTabs.module.sass'
 import {useDispatch} from "react-redux";
+import cx from "classnames"
 import {sortByFlightDuration, sortByLowestPrice} from "../../redux/tickets/actionsCreators";
 
 const SortTabs = () => {
     const dispatch = useDispatch()
+    const [tabs, setTabs] = useState({
+        price: false,
+        duration: false
+    });
     const sortByPrice = (): void => {
+        setTabs({price: true, duration: false})
         dispatch(sortByLowestPrice())
     }
     const sortByDuration = (): void => {
+        setTabs({price: false, duration: true})
         dispatch(sortByFlightDuration())
     }
     return (
         <div className={style.container}>
-            <div onClick={sortByPrice} className={style.tab}>Самый дешевый</div>
-            <div onClick={sortByDuration} className={style.tab}>Самый быстрый</div>
+            <div onClick={sortByPrice} className={cx(style.tab, {
+                [style.activeTab]: tabs.price
+            })}>Самый дешевый
+            </div>
+            <div onClick={sortByDuration} className={cx(style.tab, {
+                [style.activeTab]: tabs.duration
+            })}>Самый быстрый
+            </div>
             <div className={style.tab}>Оптимальный</div>
         </div>
     );
